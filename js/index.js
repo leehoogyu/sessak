8ba60e117b789e43fc536c29486488235350e1f6
leehoogyu <77268841+leehoogyu@users.noreply.github.com>
2021-02-10 17:07:42 +0900

    ver. 3

--- js/index.js
+++ js/index.js
@@ -1,3 +1,50 @@
+var ww = $(window).width()
+console.log(ww)
+
+var elDepLi = document.querySelectorAll('#nav .depth1 > li')
+if ( ww>=769 ) {
+    // $('#nav .depth1 > li').hover( 
+    //     function(){
+    //         $(this).addClass('on')
+    //     }, 
+    //     function(){
+    //         $(this).removeClass('on')
+    //     }
+    // )
+    for (var i=0; i<elDepLi.length; i++) {
+        elDepLi[i].addEventListener('mouseover', function(){
+            this.classList.add('on')
+        })
+        elDepLi[i].addEventListener('mouseout', function(){
+            this.classList.remove('on')
+        })
+    }
+} else {
+    // $('#nav .depth1 > li').on('click', function(){
+    //     $(this).toggleClass('on')
+        // if (  !$(this).hasClass('on')  ) { 
+        //     $(this).addClass('on')
+        // } else {
+        //     $(this).removeClass('on')
+        // }
+    //     $(this).siblings().removeClass('on')
+    // })
+    for (var i=0; i<elDepLi.length; i++) {
+        elDepLi[i].addEventListener('click', function(){
+            if (  !this.classList.contains('on')  ) { 
+                this.classList.add('on')
+            } else {
+                this.classList.remove('on')
+            }
+
+        })
+        
+    }
+
+}
+
+
+
 $(".slide-group").slick({
     autoplay: true, // 자동재생
     autoplaySpeed: 3000, // 간격시간
@@ -7,13 +54,55 @@ $(".slide-group").slick({
     nextArrow: '<button class="next"><i class="fas fa-angle-right"></i></button>',
 })
 
-$('.main .playstop').on('click', function(){
-    var $ibutton = $(this).find('i')
-    if ( $ibutton.hasClass('fa-pause') ) {
-        $('.main .slide-group').slick('slickPause')
-        $ibutton.removeClass('fa-pause').addClass('fa-play')
-    } else{
-        $('.main .slide-group').slick('slickPlay')
-        $ibutton.removeClass('fa-play').addClass('fa-pause')
+$('article:nth-child(1) .plpa').on('click', function(){
+    var ibtn = $(this).find('i')
+    if ( ibtn.hasClass('fa-pause') ) {
+        $('.slide-group').slick('slickPause')
+        ibtn.removeClass('fa-pause').addClass('fa-play')
+    } else {
+        $('.slide-group').slick('slickPlay')
+        ibtn.removeClass('fa-play').addClass('fa-pause')
     }
 })
+
+$('#header .open').on('click', function(){
+    $(this).next().css({
+        display:'block'
+    })
+    $(this).css({
+        display:'none'
+    })
+    $(this).next().next().css({
+        display:'block'
+    })
+})
+$('#header .close').on('click', function(){
+    $(this).prev().css({
+        display:'none'
+    })
+    $(this).css({
+        display:'none'
+    })
+    $(this).prev().prev().css({
+        display:'block'
+    })
+})
+
+// p.326 : 이벤트 추가 mouseenter, mouseleave
+// $('#nav .depth1 > li').on('mouseenter', function(){
+//     $(this).addClass('on')
+// })
+// $('#nav .depth1 > li').on('mouseleave', function(){
+//     $(this).removeClass('on')
+// })
+
+
+$(window).on('scroll', function(){
+    var sct = $(this).scrollTop()
+    if (sct>=10 && !$('#header').hasClass('on')) {
+        $('#header').addClass('on')
+    } else if (sct<10 && $('#header').hasClass('on')) {
+        $('#header').removeClass('on')
+    }
+})
+
